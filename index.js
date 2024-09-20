bdc0740fee5d6e740b09585e10d6b760bd7a04b1
Rahul P <rahul.cs23@bitsathy.ac.in>
2024-09-20 16:18:31 +0530

    edited

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-function AppBar() { }
-// hello world hello
-=======
-function AppBar() {
-    // hello
-    function deepak(){
+function header(){
+    function come over()
+    { 
 
     }
 }
->>>>>>> fa8034c0dee5fa893361dee287b1f6d3233e8e54
